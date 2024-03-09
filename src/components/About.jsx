b5a82c7daebc75bd23da88816463570d7025abfb
itsramishali <112942711+itsramishali@users.noreply.github.com>
2024-03-09 14:42:11 +0500

    up

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled web developer with experience in JavaScript, and expertise
+        I'm a skilled web developer based in Lahore with experience in JavaScript, and expertise
         in frameworks like React, Next.js, and Node.js. I'm a quick learner and collaborate closely with clients to
         create efficient, responsive, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
