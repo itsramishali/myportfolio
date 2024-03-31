7f2a12d28e2b17168eceb456ab6d7bcd267f872e
itsramishali <112942711+itsramishali@users.noreply.github.com>
2024-03-31 09:26:22 +0500

    chn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas} from './components';
 
 // import { Hero, Navbar, } from './components';
 
@@ -14,7 +14,7 @@ const App = () => {
           </div>
             <About/> 
             <Experience/> 
-            <Tech/>  
+            {/* <Tech/>   */}
             <Works/> 
             <Feedbacks/> 
             <div className='relative z-0'>
